8e45a193cc8799b4bb5165a8e3ec2ccf4701b16b
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2020-06-11 08:44:36 +0000

    set up api key and id, react useState destructure

--- src/Recipe/Main/Main.js
+++ src/Recipe/Main/Main.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 
 const Main = () => {
+    const APP_ID = '67d62c8a';
+    const APP_KEY = '34f530f5e5c5c4ee8d0b6ea08b047289';
+
+    const [recipes, setRecipes] = useState([]);
+    const [search, setSearch] = useState('');
+    const [query, setQuery] = useState('fish');
+
     return(
         <main>
             <h1 className="mainTitle">Search your favorite recipe now!!!</h1>
